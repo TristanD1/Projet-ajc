4c70a60d8145a3c6af2d81cb920ccbba12f58b46
MickaAile <ringuet-mickael@wanadoo.fr>
2021-08-12 14:42:33 +0200

    maj

--- Projet/src/main/java/Projet/Projet/DAO/Jpa/QueteDaoJpa.java
+++ Projet/src/main/java/Projet/Projet/DAO/Jpa/QueteDaoJpa.java
@@ -1,5 +1,22 @@
 package Projet.Projet.DAO.Jpa;
 
+import java.util.List;
+
+import Projet.Projet.DAO.IQueteDao;
+import model.Quete;
+
 public class QueteDaoJpa extends AbstractDaoJpa<Quete, Integer> implements IQueteDao{
+
+	@Override
+	public List<Quete> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Quete findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
