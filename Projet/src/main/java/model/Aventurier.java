f2cc7eea3f01e587bdb3e5582be0cf061aa816dd
MickaAile <ringuet-mickael@wanadoo.fr>
2021-08-12 14:34:53 +0200

    maj

--- Projet/src/main/java/model/Aventurier.java
+++ Projet/src/main/java/model/Aventurier.java
@@ -3,6 +3,7 @@ package model;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,8 +11,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+@Entity
+@Table(name = "aventurier")
 public class Aventurier {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
