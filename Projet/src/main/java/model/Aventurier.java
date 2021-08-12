42e7e6fabc93b4521a0f9a13a3a6d6e8f4f84f7e
MickaAile <ringuet-mickael@wanadoo.fr>
2021-08-12 14:24:20 +0200

    maj

--- Projet/src/main/java/model/Aventurier.java
+++ Projet/src/main/java/model/Aventurier.java
@@ -2,10 +2,35 @@ package model;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.UniqueConstraint;
+
 public class Aventurier {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "AVE=ID")
+	private int id;
+
+	@Column(name = "AVE_NOM", nullable = false)
 	private String nom;
+
+	@Column(name = "AVE_EXP")
 	private int experience;
+
+	@ManyToOne
+	@Column(name = "AVE_QTE")
 	private Quete quete;
+
+	@ManyToMany
+	@JoinTable(name = "Attribution Equipement", joinColumns = @JoinColumn(name = "AVE_ID", referencedColumnName = "AVE_ID") , inverseJoinColumns = @JoinColumn(name = "EQU_ID", referencedColumnName = "EQU_ID") , uniqueConstraints = @UniqueConstraint(columnNames = {
+			"AVE_ID", "EQU_ID" }) )
 	private List<Equipement> equipements;
 
 	public String getNom() {
--- Projet/src/main/java/model/Equipement.java
+++ Projet/src/main/java/model/Equipement.java
@@ -1,5 +1,49 @@
 package model;
 
-public class Equipement {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+public class Equipement {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "EQU_NOM")
+	private String nom;
+
+	@ManyToMany(mappedBy = "equipements")
+	private List<Aventurier> aventuriers;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public List<Aventurier> getAventuriers() {
+		return aventuriers;
+	}
+
+	public void setAventuriers(List<Aventurier> aventuriers) {
+		this.aventuriers = aventuriers;
+	}
+
+	public Equipement() {
+
+	}
 }
