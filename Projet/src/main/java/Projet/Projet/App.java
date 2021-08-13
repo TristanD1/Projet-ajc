90e03c0cab924d02b7cd8a16c898cb5b490c7029
MickaAile <ringuet-mickael@wanadoo.fr>
2021-08-13 12:25:22 +0200

    maj

--- Projet/src/main/java/Projet/Projet/App.java
+++ Projet/src/main/java/Projet/Projet/App.java
@@ -1,7 +1,11 @@
 package Projet.Projet;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import javax.persistence.EntityManager;
 
 import Projet.Projet.DAO.Jpa.AbstractDaoJpa;
 import Projet.Projet.DAO.Jpa.AventurierDaoJpa;
@@ -14,24 +18,28 @@ import model.Etat;
 
 public class App {
 	public static void main(String[] args) {
-		CreateAventurier("Aragorn", 15);
-		CreateAventurier("Legolas", 15);
-		CreateAventurier("Bilbon", 1);
-		CreateAventurier("Gimli", 15);
-		CreateAventurier("Gandalf", 100);
-		
-		CreateEquipement("Dague", 10);
-		CreateEquipement("Epée", 30);
-		CreateEquipement("Arc", 20);
-		CreateEquipement("Fronde", 10);
-		CreateEquipement("Hache", 40);
-		CreateEquipement("Sortilege", 50);
-
-		CreateQuete("Protection de convoi", 500);
-		CreateQuete("Protection de village", 1000);
-		CreateQuete("Protection de Minas Tirith", 5000);
-		CreateQuete("Attaque d'Isengard", 3000);
-		CreateQuete("Destruction de l'Anneau Unique", 100000);
+		// CreateAventurier("Aragorn", 15);
+		// CreateAventurier("Legolas", 15);
+		// CreateAventurier("Bilbon", 1);
+		// CreateAventurier("Gimli", 15);
+		// CreateAventurier("Gandalf", 100);
+		//
+		// CreateEquipement("Dague", 10);
+		// CreateEquipement("Epée", 30);
+		// CreateEquipement("Arc", 20);
+		// CreateEquipement("Fronde", 10);
+		// CreateEquipement("Hache", 40);
+		// CreateEquipement("Sortilege", 50);
+		//
+		// CreateQuete("Protection de convoi", 500);
+		// CreateQuete("Protection de village", 1000);
+		// CreateQuete("Protection de Minas Tirith", 5000);
+		// CreateQuete("Attaque d'Isengard", 3000);
+		// CreateQuete("Destruction de l'Anneau Unique", 100000);
+		//
+		// AssocierEquipementAventurier(3, 1);
+		//
+		// AssocierAventurierQuete(1, 3);
 
 		AbstractDaoJpa.close();
 	}
@@ -59,5 +67,25 @@ public class App {
 		maQuete.setProbaReussite(reussite);
 
 		new QueteDaoJpa().save(maQuete);
+	}
+
+	public static void AssocierEquipementAventurier(int aventurierId, int equipementId) {
+		Equipement monEquipement = new EquipementDaoJpa().findById(equipementId);
+		Aventurier monAventurier = new AventurierDaoJpa().findById(aventurierId);
+
+		if (monEquipement.getAventurier() == null) {
+			monEquipement.setAventurier(monAventurier);
+			new EquipementDaoJpa().save(monEquipement);
+		}
+	}
+
+	public static void AssocierAventurierQuete(int aventurierId, int queteId) {
+		Aventurier monAventurier = new AventurierDaoJpa().findById(aventurierId);
+		Quete maQuete = new QueteDaoJpa().findById(queteId);
+
+		if (monAventurier.getQuete() == null) {
+			monAventurier.setQuete(maQuete);
+			new AventurierDaoJpa().save(monAventurier);
+		}
 	}
 }
