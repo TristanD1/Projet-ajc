75c95cc275bbd8c4e88f23638b44e19f80de6a41
MickaAile <ringuet-mickael@wanadoo.fr>
2021-08-13 14:40:50 +0200

    maj

--- Projet/src/main/java/Projet/Projet/App.java
+++ Projet/src/main/java/Projet/Projet/App.java
@@ -1,11 +1,6 @@
 package Projet.Projet;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
-import javax.persistence.EntityManager;
+import java.util.Random;
 
 import Projet.Projet.DAO.Jpa.AbstractDaoJpa;
 import Projet.Projet.DAO.Jpa.AventurierDaoJpa;
@@ -18,28 +13,9 @@ import model.Etat;
 
 public class App {
 	public static void main(String[] args) {
-		// CreateAventurier("Aragorn", 15);
-		// CreateAventurier("Legolas", 15);
-		// CreateAventurier("Bilbon", 1);
-		// CreateAventurier("Gimli", 15);
-		// CreateAventurier("Gandalf", 100);
-		//
-		// CreateEquipement("Dague", 10);
-		// CreateEquipement("Epée", 30);
-		// CreateEquipement("Arc", 20);
-		// CreateEquipement("Fronde", 10);
-		// CreateEquipement("Hache", 40);
-		// CreateEquipement("Sortilege", 50);
-		//
-		// CreateQuete("Protection de convoi", 500);
-		// CreateQuete("Protection de village", 1000);
-		// CreateQuete("Protection de Minas Tirith", 5000);
-		// CreateQuete("Attaque d'Isengard", 3000);
-		// CreateQuete("Destruction de l'Anneau Unique", 100000);
-		//
-		// AssocierEquipementAventurier(3, 1);
-		//
-		// AssocierAventurierQuete(1, 3);
+
+		AssocierAventurierQuete(1, 1);
+		EnvoyerEnMission(1);
 
 		AbstractDaoJpa.close();
 	}
@@ -69,23 +45,62 @@ public class App {
 		new QueteDaoJpa().save(maQuete);
 	}
 
-	public static void AssocierEquipementAventurier(int aventurierId, int equipementId) {
+	public static void AssocierEquipementAventurier(int equipementId, int aventurierId) {
 		Equipement monEquipement = new EquipementDaoJpa().findById(equipementId);
 		Aventurier monAventurier = new AventurierDaoJpa().findById(aventurierId);
 
-		if (monEquipement.getAventurier() == null) {
-			monEquipement.setAventurier(monAventurier);
-			new EquipementDaoJpa().save(monEquipement);
-		}
+		monEquipement.setAventurier(monAventurier);
+		new EquipementDaoJpa().save(monEquipement);
 	}
 
 	public static void AssocierAventurierQuete(int aventurierId, int queteId) {
 		Aventurier monAventurier = new AventurierDaoJpa().findById(aventurierId);
 		Quete maQuete = new QueteDaoJpa().findById(queteId);
 
-		if (monAventurier.getQuete() == null) {
-			monAventurier.setQuete(maQuete);
-			new AventurierDaoJpa().save(monAventurier);
+		monAventurier.setQuete(maQuete);
+		new AventurierDaoJpa().save(monAventurier);
+	}
+
+	public static void EnvoyerEnMission(int queteId) {
+		Quete maQuete = new QueteDaoJpa().findById(queteId);
+
+		if (maQuete.getAventuriers().size() > 0) {
+			int proba = 0;
+
+			for (Aventurier a : maQuete.getAventuriers()) {
+				proba += a.getExperience();
+				for (Equipement e : a.getEquipements()) {
+					proba += e.getBonus();
+				}
+			}
+
+			System.out.println("bonus : " + proba);
+			System.out.println("difficulté : " + maQuete.getProbaReussite());
+			double r = new Random().nextDouble();
+			System.out.println(r);
+
+			// Faire le test de reussite
+			if (r < (double) proba / (proba + maQuete.getProbaReussite())) {
+				// Changer le status de la quete si reussite
+				maQuete.setEtat(Etat.ACHEVEE.toString().toLowerCase());
+				new QueteDaoJpa().save(maQuete);
+
+				for (Aventurier a : maQuete.getAventuriers()) {
+					a.setExperience(a.getExperience() + maQuete.getProbaReussite());
+				}
+
+				System.out.println("Quete réussie");
+			} else {
+				System.out.println("Quete échouée");
+			}
+
+			// desassocier les aventuriers
+			for (Aventurier a : maQuete.getAventuriers()) {
+				a.setQuete(null);
+				new AventurierDaoJpa().save(a);
+			}
+		} else {
+			System.out.println("pas d'aventuriers associés");
 		}
 	}
 }
