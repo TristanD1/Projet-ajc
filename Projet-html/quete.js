340b9c9a6d68ad6482813ce89151dadcf74c7e8c
Jordanloubeau <jordan.loubeau@gmail.com>
2021-08-25 15:08:12 +0200

    popup

--- Projet-html/quete.js
+++ Projet-html/quete.js
@@ -3,11 +3,11 @@ for (let i = 1; i < 100; i++) {
     document.querySelector(`#ajout${i}`).addEventListener('click', () => {
        
         if (document.querySelector(`#ajout${i}`).innerHTML == `<a class="btn btn-danger" onclick="onClick()">Enlever</a>`) {
-            document.querySelector(`#statut${i}`).innerHTML = '<i class="bi bi-person-check-fill"></i>';
+            document.querySelector(`#statut${i}`).innerHTML = '<i class="bi bi-person-x-fill"></i>';
             document.querySelector(`#ajout${i}`).innerHTML = '<a class="btn btn-danger" onclick="onClick()">Ajouter</a>';
         }
         else {
-            document.querySelector(`#statut${i}`).innerHTML = '<i class="bi bi-person-x-fill"></i>';
+            document.querySelector(`#statut${i}`).innerHTML = '<i class="bi bi-person-check-fill"></i>';
             document.querySelector(`#ajout${i}`).innerHTML = '<a class="btn btn-danger" onclick="onClick()">Enlever</a>';
         }
 
