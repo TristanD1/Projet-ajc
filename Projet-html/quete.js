17f95f312543c1e5baae96d4e5943d6be3e27437
Jordanloubeau <jordan.loubeau@gmail.com>
2021-08-25 14:28:17 +0200

    popup

--- Projet-html/quete.js
+++ Projet-html/quete.js
@@ -1,4 +1,30 @@
-document.querySelector('#btn-choix').addEventListener('click', () =>{
-    window.open("pop-up-quete.html", "nomdelafenetrepopup", "height=XXX, width=XXX, menubar='yes|no', toolbar='yes|no', location='yes|no', status='yes|no', scrollbars='yes|no'");
-});
+var clicks = 1;
+
+function onClick() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+
+
+for (let i = 1; i < 100; i++) {
+
+    document.querySelector(`#ajout${i}`).addEventListener('click', () => {
+
+        if (clicks % 2 == 0) {
+            document.querySelector(`#statut${i}`).innerHTML = '<i class="bi bi-person-check-fill"></i>';
+        }
+        else {
+            document.querySelector(`#statut${i}`).innerHTML = '<i class="bi bi-person-x-fill"></i>'
+        }
+
+    })
+    document.querySelector(`#close`).addEventListener('click', () =>{
+        clicks=1
+    });
+
+}
+
+
+
 
