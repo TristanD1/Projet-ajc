6000f0366d6c9e4de35a629e60cf60ca01a8e83e
Jordanloubeau <jordan.loubeau@gmail.com>
2021-08-26 09:32:34 +0200

    popup

--- Projet-html/quete.js
+++ Projet-html/quete.js
@@ -1,38 +1,30 @@
-for (let i = 1; i <= 3; i++) {
-//elementHTML.getAttribute('attr')
-//elementHTML.setAttribute('attr', 'value')
-    document.querySelector(`#ajout${i}`).addEventListener('click', () => {
-       
-        if (document.querySelector(`#ajout${i}`).innerHTML == `<a class="btn btn-danger" onclick="onClick()">Enlever</a>`) {
-            document.querySelector(`#statut${i}`).innerHTML = '<i class="bi bi-person-x-fill"></i>';
-            document.querySelector(`#ajout${i}`).innerHTML = '<a class="btn btn-danger" onclick="onClick()">Ajouter</a>';
+var listBtnAjouter = document.querySelectorAll("#ajout");
+
+for (let btn of listBtnAjouter) {
+    btn.addEventListener('click', () => {
+
+        if (btn.innerHTML == 'Enlever') {
+            document.querySelector(`#statut`).innerHTML = '<i class="bi bi-person-x-fill"></i>';
+            btn.innerHTML = 'Ajouter';
         }
+
         else {
-            document.querySelector(`#statut${i}`).innerHTML = '<i class="bi bi-person-check-fill"></i>';
-            document.querySelector(`#ajout${i}`).innerHTML = '<a class="btn btn-danger" onclick="onClick()">Enlever</a>';
+            document.querySelector(`#statut`).innerHTML = '<i class="bi bi-person-check-fill"></i>';
+            btn.innerHTML = 'Enlever'
         }
         document.querySelector(`#close`).addEventListener('click', () => {
-            document.querySelector(`#statut${i}`).innerHTML = '<i class="bi bi-person-x-fill"></i>';
-            document.querySelector(`#ajout${i}`).innerHTML = '<a class="btn btn-danger" onclick="onClick()">Ajouter</a>';
-        });
-
+            document.querySelector(`#statut`).innerHTML = '<i class="bi bi-person-x-fill"></i>';
+            btn.innerHTML = 'Ajouter';
+        })
     })
-}
+};
 
 
-document.querySelector(`#btn-choix1`).addEventListener('click', () =>{
-    document.querySelector(`#intituleQuete`).textContent = document.querySelector(`#intitule1`).textContent;
-})
+// document.querySelector(`#btn-choix1`).addEventListener('click', () =>{
+//     document.querySelector(`#intituleQuete`).textContent = document.querySelector(`#intitule1`).textContent;
+// })
 
-document.querySelector('#eq1').addEventListener('click', () =>{
-    document.querySelector('#dropdownMenuButton1').textContent = document.querySelector('#eq1').textContent;
+// document.querySelector('#eq1').addEventListener('click', () =>{
+//     document.querySelector('#dropdownMenuButton1').textContent = document.querySelector('#eq1').textContent;
 
-})
-// function myfunction(){
-// var x = document.getElementById("ajout").getAttribute("name");
-
-
-
-// console.log(x);
-// }
-// myfunction();
+// })
