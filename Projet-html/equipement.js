d58b58835e01fac7591e71dec0a78f41b600bd42
TristanD1 <tristan.dufay@hotmail.fr>
2021-08-26 13:44:48 +0200

    x

---
+++ Projet-html/equipement.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#tooltipEpee').tooltip({
+        title: "<h3>Epée</h3> <img src=dev/common-sword.jpg> <p> <i> Ce n'est qu'une épée tout ce qu'il y a de plus commune mais cela ne l'empêchera pas de faire des ravages dans les mains d'un expert </i> </p>",
+         html: true,
+          placement: "bottom"
+    }); 
+    $('#tooltipDague').tooltip({
+        title: "<h3>Dague</h3> <img src=dev/common-daguer.jpg> <p> <i> Petite mais affutée, son poid très faible en fait une arme parfaite pour les personnes de faible constitution </i> </p>",
+         html: true,
+          placement: "bottom"
+    }); 
+  });
+
